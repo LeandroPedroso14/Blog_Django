7994c1970a175291775e257317b553ab6e0d0dad
Leandro Pedroso <lpedroso64@outlook.com>
2023-01-31 11:20:39 -0300

    indentation code

--- blog_tec/blog/models.py
+++ blog_tec/blog/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-
 class Post(models.Models):
-    title = models.CharField(max_length=255)
-    resumo = models.CharField(max_length=255)
+   title = models.CharField(max_length=255)
+   resumo = models.CharField(max_length=255)
