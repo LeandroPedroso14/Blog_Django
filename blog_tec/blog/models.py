83e09ac4531af5c83a5fe216b6816625c2dcdd05
Leandro Pedroso <lpedroso64@outlook.com>
2023-01-31 11:45:11 -0300

    importing user , creating author

--- blog_tec/blog/models.py
+++ blog_tec/blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -5,3 +6,4 @@ class Post(models.Models):
    title = models.CharField(max_length=255)
    resumo = models.CharField(max_length=255)
    content = models.TextField()
+   author = models.ForeignKey(User, on_delete=models.PROTECT)
