141fd1663e89da70649f317f2e5f643bff489062
Leandro Pedroso <lpedroso64@outlook.com>
2023-02-08 14:16:18 -0300

    creating function to return a word

--- core/models.py
+++ core/models.py
@@ -11,3 +11,6 @@ class Tech(models.Model):
     begin_date = models.DateTimeField(auto_now_add=True)
     photo = models.ImageField()
     active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.id)
