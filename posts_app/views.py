549e30908e728754def0a3ee00698e5b4816d1d6
Leandro Pedroso <lpedroso64@outlook.com>
2023-02-11 02:38:48 -0300

    defining function for list request.

--- posts_app/views.py
+++ posts_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from posts_app.models import Posts
 
 # Create your views here.
+
+
+def post_list(request):
+    template_name = 'post-list.html'
+    posts = Posts.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, template_name, context)
