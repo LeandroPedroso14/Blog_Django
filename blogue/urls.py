f389cb586997e086f4a9bef2ad2b4cee12952904
Leandro Pedroso <lpedroso64@outlook.com>
2023-02-11 01:21:12 -0300

    adding tags

--- blogue/urls.py
+++ blogue/urls.py
@@ -21,3 +21,8 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATIC_ROOT)  # Adicionar
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)  # Adicionar
